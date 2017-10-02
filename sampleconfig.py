1f69ad05bb48c1d3a185df9239356fa43f3632b3
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2017-10-03 01:50:23 +0800

    Add sample config file

---
+++ sampleconfig.py
@@ -0,0 +1,19 @@
+# Sample config file for user to input their own access tokens and keys
+
+uber = {
+    'client_id': '',
+    'client_secret': '',
+    'server_token': ''
+}
+
+telegram = {
+    'bot_id': ''
+}
+
+google = {
+    'places_key': ''
+}
+
+tff = {
+    'api_key': ''
+}
